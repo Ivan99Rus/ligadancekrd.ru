e24e5b3bf051f252eaef5cf35db5b3d655f511ac
Ivan <ivananapa@mail.ru>
2020-05-27 11:13:28 +0300

    feat: add new photo in progress slider and refact mail.php file

--- mail.php
+++ mail.php
@@ -1,19 +1,71 @@
-<?php 
-$name = $_POST["user-name"];
-$phone = $_POST["user-phone"];
-$email = $_POST["user-email"];
+<?php
+require 'root/root.php';
 
-$to  = "ivananapa@mail.ru" ; 
+$modal__userName = $_POST['modal__userName'];
+$modal__userPhone = $_POST['modal__userPhone'];
+$modal__userEmail = $_POST['modal__userEmail'];
 
-$subject = "Заявка с сайта Liga Dance"; 
+$hero__userName = $_POST['hero__userName'];
+$hero__userPhone = $_POST['hero__userPhone'];
 
-$message = '<strong>' . $name . '</strong> оставил(-а) заявку, Вы пожете связаться по номеру <strong> ' . $phone . '</strong>';
-if (!empty($email)) 
-  $message .= '<br>Email: ' . $email;
+$questions__userName = $_POST['questions__userName'];
+$questions__userPhone = $_POST['questions__userPhone'];
 
-$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
-$headers .= "From: ligadancekrd.ru\r\n"; 
-$headers .= "Reply-To: no-reply\r\n"; 
+$userName = '';
+$userPhone = '';
+$userEmail = '';
 
-mail($to, $subject, $message, $headers); 
-?>
+if (!empty($modal__userName)) {
+    $userName = $modal__userName;
+    $userPhone = $modal__userPhone;
+    $userEmail = $modal__userEmail;
+} else if(!empty($hero__userName)) {
+    $userName = $hero__userName;
+    $userPhone = $hero__userPhone;
+} else if(!empty($questions__userName)) {
+    $userName = $questions__userName;
+    $userPhone = $questions__userPhone;
+}
+
+if (!empty($userEmail)) {
+    $body = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+} else {
+    $body = "Имя пользователя: ${userName}, его телефон: ${userPhone}";
+}
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ligadancekrd@gmail';                     // SMTP username
+    $mail->Password   = 'ligadancekrd@gmail.compass2pass';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+    
+    //Recipients
+    $mail->setFrom('ligadancekrd@gmail.com');
+    $mail->addAddress('ivananapa@mail.ru');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = $body;
+
+    if($mail->send()) {
+        echo 'ok';
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
